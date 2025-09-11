f9908576b373636bb0d597a121193557a30af642
Nic Nicolo <nicnicolo@MacBookAir.lan>
2025-09-10 22:43:40 -0400

    Add ScrollToTop component to fix navigation scroll position

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from 'react-router';
 import Header from './layout/Header';
 import Footer from './layout/Footer';
+import ScrollToTop from './ScrollToTop';
 
 export default function Layout() {
   return (
     <div className="min-h-screen bg-white">
+      <ScrollToTop />
       <Header />
       <Outlet />
       <Footer />
